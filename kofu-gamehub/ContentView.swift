c9e428b225a89b086a1d60109ac03e79bb25d027
Kofusu <kofusu@protonmail.com>
2025-05-29 18:14:56 +0700

    Initial Commit

---
+++ kofu-gamehub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  kofu-gamehub
+//
+//  Created by Hendratara Pratama on 29/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ kofu-gamehub/kofu_gamehubApp.swift
@@ -0,0 +1,17 @@
+//
+//  kofu_gamehubApp.swift
+//  kofu-gamehub
+//
+//  Created by Hendratara Pratama on 29/05/25.
+//
+
+import SwiftUI
+
+@main
+struct kofu_gamehubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
